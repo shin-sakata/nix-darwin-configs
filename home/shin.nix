c6fc68ca8de194e353576d37532a62dfb42fbc12
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2026-01-13 09:33:44 +0900

    Fix escape character in identityAgent path for 1Password configuration

--- home/shin.nix
+++ home/shin.nix
@@ -21,7 +21,7 @@
     enable = true;
     enableDefaultConfig = false;
     matchBlocks."*" = {
-      identityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      identityAgent = "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock";
     };
   };
 
